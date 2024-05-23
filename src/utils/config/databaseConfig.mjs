3aeab9ad43832a9e6849bfa27829376b129169a4
Mathenriques <math.marqui@gmail.com>
2024-05-23 14:29:20 -0300

    Add env variables

--- src/utils/config/databaseConfig.mjs
+++ src/utils/config/databaseConfig.mjs
@@ -1,13 +1,16 @@
 import { DataSource } from 'typeorm';
 import postSchema from '../database/entitities/postEntity.mjs';
 
+import dotenv from 'dotenv';
+dotenv.config();
+
 const dataSourceConfig = new DataSource({
   type: 'postgres',
-  host: 'dbpostgres',
+  host: process.env.DB_HOST,
   port: 5432,
-  username: 'postgres',
-  password: 'postgres',
-  database: 'postgres',
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
   entities: [postSchema],
   // synchronize: true, // Not recommend, but its a test so we keep
 });
